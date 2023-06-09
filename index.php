d7c6307585110c07fbcab21cd0f18422a1c1173e
Hazoofa <134072497+Hazoofa@users.noreply.github.com>
2023-06-09 22:22:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>shop watches</title>
+
+    <link rel="stylesheet" href="css/sstyle.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo">Watch</a>
+
+    <div id="menu" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <ul>
+            <li><a class="active" href="#home">home</a></li>
+            <li><a href="#core">core</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#features">features</a></li>
+           
+            <li><a href="add_product.php">Login</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h1>كل ماهو جديد  اطلب منتجك</h1>
+        <p>لتسويق  منتجك والبحث عن منتجات  جديده ساعات انيقه وذكيه اظافه منتجات حصريه </p>
+     <!-- <p>يوجد لدينا جميع انواع الساعات الذكيه والانيقه والحصريه  اطلب او اتصل نصل </p> -->
+        <!-- <a href="#"><button class="btn">order now</button></a> -->
+    </div>
+
+    <div class="image">
+        <img src="../imege/Best-All-Black-Watches-0-Hero-1074x711.jpg" alt="">
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- core features section starts  -->
+
+<section class="core" id="core">
+
+<h1 class="heading">المميزات الاساسيه للمنتجاتنا </h1>
+
+<div class="row">
+
+    <div class="image">
+        <img src="../imege/80d491faafa433baa12fc81a27b4c3ea907e7066.jpg" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3> <i class="fas fa-user"></i> user friendly </h3>
+
+          
+            <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+        </div>
+
+        <div class="box">
+            <h3> <i class="fas fa-shield-alt"></i> high security </h3>
+
+    
+            <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+            
+        </div>
+
+        <div class="box">
+            <h3> <i class="fas fa-sync-alt"></i> unlimited updates </h3>
+           
+            <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+            
+        </div>
+
+        <div class="box">
+            <h3> <i class="fas fa-magic"></i> unlimited features </h3>
+           
+         
+            <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+            
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+<!-- core features section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading">مميزات هذ المتج</h1>
+
+<div class="row">
+
+    <div class="image">
+        <img src="../imege/60174d53-f6fc-4534-8216-751f41278180.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>الساعات الذكيه معك مدى الحياه مميزه من كل جانب.</h3>
+       
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+        <p> <i class="fas fa-check-circle"></i> تعمل بالطاقه الشمسيه وفصل الرياح  </p>
+        <p> <i class="fas fa-check-circle"></i> تعمل بالطاقه الشمسيه وفصل الرياح</p>
+        <p> <i class="fas fa-check-circle"></i>تعمل بالطاقه الشمسيه وفصل الرياح</p>
+        <a href="#"><button class="btn">learn more</button></a>
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- features section starts  -->
+
+<section class="features" id="features">
+
+<h1 class="heading">Features are Available in The Watch</h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <i class="fas fa-bell"></i>
+        <h3>Notification alert</h3>
+        
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-comments"></i>
+        <h3>live chat</h3>
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-heartbeat"></i>
+        <h3>heart beat tracker</h3>
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-map-marker-alt"></i>
+        <h3>GPS tracker</h3>
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-wifi"></i>
+        <h3>wifi facility</h3>
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+    <div class="box">
+        <i class="fas fa-cloud-sun"></i>
+        <h3>weather updates</h3>
+        <p>سهل الاستخدام وويحدد اسرعة ضربات القلب ومكان القبله ومنبه للاذان والاقامه سمس وممتع انيق وجميل  جذاب</p>
+
+    </div>
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
